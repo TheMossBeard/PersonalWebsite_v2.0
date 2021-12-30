5ca80ae8604af0951f9baca82f91b59b3c2a0595
Your NameTheMossBeard <jessew529@gmail.com>
2021-12-29 21:01:55 -0800

    wasting time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,20 @@
 
 $(document).ready(function () {
-    $('.second-button').on('click', function () {
-      $('.animated-icon2').toggleClass('open');
-    });
+  $('.second-button').on('click', function () {
+    $('.animated-icon2').toggleClass('open');
   });
+});
 
+var btns = $("#nav-section navbar nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click",
+    function () {
+      var current = document
+        .getElementsByClassName("active");
+
+      current[0].className = current[0]
+        .className.replace(" active", "");
+
+      this.className += " active";
+    });
+}
