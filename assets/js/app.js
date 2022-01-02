81e0aebe187999999776d2f5886daa52ed99e7cd
Jesse Whiting <jessew529@gmail.com>
2022-01-02 11:29:22 -0800

    adjusted footer & some styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,5 +6,10 @@ $(document).ready(function () {
 });
 
 $(window).on('resize', function () {
-  if (window.innerWidth > 768) $('.navbar-collapse').collapse('hide')
+  if (window.innerWidth > 768) {
+    $('.navbar-collapse').collapse('hide');
+    //if ($('.animated-icon2').collapse == 'open')
+    //  $('.animated-icon2').toggleClass('open'); need to reset animated button from X
+  }
+
 })
