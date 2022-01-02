0782dfec37dbd886f799380339837e2658fa4ebf
Your NameTheMossBeard <jessew529@gmail.com>
2022-01-01 22:28:55 -0800

    fixed dropdown closing on window resize

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@
 $(document).ready(function () {
   $('.second-button').on('click', function () {
     $('.animated-icon2').toggleClass('open');
-  });
+  }); //check for show, if true, hide else open so it is smoother
 });
 
-
+$(window).on('resize', function () {
+  if (window.innerWidth > 768) $('.navbar-collapse').collapse('hide')
+})
